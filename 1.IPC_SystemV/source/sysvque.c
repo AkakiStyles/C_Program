864b123d265d765cb4adabcf9b71569f4954da54
Steve <steve@aitc.com.tw>
2024-07-29 17:27:08 +0800

    (ADD) IPC and autotool

---
+++ 1.IPC_SystemV/source/sysvque.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/ipc.h> 
+#include <sys/msg.h> 
+#include <memory.h>
+#include "onepurch.h"
+
+
+int main() {
+
+char *s = "abcde";
+char *t = "12345";
+
+#if defined(LINUX) && defined(ANDROID)
+printf ("%s\n", s);
+#else
+printf ("%s\n", t);
+#endif
+
+return 0;
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # C_Program
+1.IPC_SystemV 
+2.Multithread
+3.TcpIpSocket   
+
+# OS
+mac
+rocky linux
+
+# Requeirment
+autoconf
+automake
+
+# How to make
+
+
